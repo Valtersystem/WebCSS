970ab7da45074ecbac0f727eac280aea11a4d783
Valtersystem <69923057+Valtersystem@users.noreply.github.com>
2021-10-15 11:40:41 -0300

    Add files via upload

---
+++ partitions/js/on-off-lamp.js
@@ -0,0 +1,11 @@
+let light = document.getElementById("light");
+let light_off = document.getElementById("light_off");
+
+function mouseOver() {
+     light.classList.toggle("on");
+}
+
+//Não está bom
+// function mouseOut(){
+//     light.classList.toggle("off");
+//}
